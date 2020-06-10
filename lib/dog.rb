7f34f080ec44bb02b31b517a28ebfba744408ef4
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-10 17:12:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,15 @@ class Dog
 def name=(name)
   @name = name
 end
-def name
-  @name
+  def name
+    @name
   end
-  def breed =(breed)
-    @breed = breed
-  end
-    def breed
-      @breed
+    def breed =(breed)
+      @breed = breed
+    end
+      def breed
+        @breed
+      end
     end
 
 snoopy = Dog.new
